f8afe2cb5c03ef7c562ce0c8cacdf2bf7448b649
changxing1999 <you@example.com>
2018-06-02 17:29:14 +0800

    c

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/css/base.css';
+
 import VueResource from 'vue-resource';
 Vue.use(VueResource)
 Vue.http.options.emulateJSON = true;
